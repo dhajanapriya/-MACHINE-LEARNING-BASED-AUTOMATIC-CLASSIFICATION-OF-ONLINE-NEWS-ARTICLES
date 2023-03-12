d9d5e1882934c9f97178ab0d141177e5b2acd474
dhajanapriya <64087430+dhajanapriya@users.noreply.github.com>
2023-03-12 06:53:51 +0000

    Add files via upload

---
+++ subscripion.php
@@ -0,0 +1,40 @@
+<?php 
+    require_once "include/header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Subscription</title>
+</head>
+<h1>Thank You For Subscribing....</h1>
+<style>
+ p{
+ 	text-align: left;
+ 	font-size: 30px;
+ }
+
+
+
+</style>
+<body>
+	<article>
+<h1>Black Widow</h1>
+<img
+  src="black.png"
+  alt="refresh" />
+  <p>Atasha Romanoff aka Black Widow made her MCU debut in 2010’s “Iron Man 2.” In the intervening 11 years, she’s joined the Avengers, saved the world half a dozen times, and — two-year-old spoiler alert — sacrificed her life in the fight against Thanos. And now she’s finally, finally the star of her own movie. “Black Widow,” from “Somersault” director Cate Shortland, catches up with Natasha (Scarlett Johansson) between the events of “Captain America: Civil War” and “Avengers: Infinity War.”</p><br>
+
+<p>Her found family in disarray and the authorities on her tail, Natasha goes into hiding but it’s not long before her other estranged family is back in her life. Her sister, Yelena (Florence Pugh), tracks her down with the news that the man that turned them both into assassins is still up to his old tricks. His organization, the Red Room, finds abandoned and underprivileged girls, takes them in, and medically and psychologically manipulates them until they are completely stripped of their agency. Dozens of women have become unfeeling killers through no fault or choice of their own.</p><br>
+
+<p>Soon, Natasha and Yelena are enlisting their parents, Melina (Rachel Weisz) and Alexei (David Harbour), in the fight against “Black Widow’s” Big Bad, who may as well be named The Patriarchy. The movie explicitly engages with themes of reproductive justice, consent, and toxic masculinity, and is all the better for it. It’s the first time in my memory that any quippy MCU conversation has revolved around forced sterilization and men’s ignorance of women’s bodies — let alone included the words “periods,” “uterus,” “ovaries,” and “fallopian tubes.” It’s glorious.</p><br>
+<p>This being a Marvel film, there are also plenty of wisecracks, fights, and explosions to be enjoyed. I’m pleased to report that in this story the three women are both the brains and the brawn of the operation, while Alexei is mostly there for comic relief. The core cast has great chemistry and the sarcastic Pugh and no-nonsense Weisz are particular standouts.</p><br>
+
+<p>Most of all, I’m happy that a film that is so unapologetically interested in women’s autonomy, physical and otherwise, was actually helmed by a woman. “Black Widow” marks the first MCU movie from a solo woman director (2019’s “Captain Marvel” was co-directed by Anna Boden and Ryan Fleck), but it won’t be the last: Chloé Zhao’s “Eternals” will be out this November and Nia DaCosta’s “Captain Marvel” sequel is expected next year.</p><br>
+<p>Between its director, its “Made for Love” and “Jessica Jones”-esque look at the ubiquitous evil of men trying to control women, and its heroine, “Black Widow” is a milestone Marvel movie. It’s just a shame that we saw a decade pass (and the main character die!) before it was released. The bright side is that Marvel has been known to resurrect characters, so perhaps Black Widow and her family will grace the big screen again in the future. After 13 years and a shitload of movies starring a revolving door of white dudes named Chris, I think the MCU owes it to us.</p><br>
+</body>
+</article>
+</html>
+<?php 
+  require_once "include/footer.php";
+?>
